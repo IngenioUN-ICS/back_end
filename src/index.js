5333150ec17c1b0a3557425dc9b763ecdc823dd2
Daniel Felipe <daneelfx@gmail.com>
2020-12-13 20:33:12 -0500

    https implementation using an auto-firmed ssl certificate: done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-const app = require( "./app" );
-require( "./database" );
+const app = require('./app')
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
 
-async function main( ) {
-  await app.listen( app.get( "port" ) );
-  console.log( "Server on port", app.get( "port" ) );
-}
-main( );
+require('./database')
+
+// const httpsOptions = {
+//   cert: fs.readFileSync(path.join(__dirname, 'config', 'ssl','server.crt')),
+//   key: fs.readFileSync(path.join(__dirname, 'config', 'ssl','server.key'))
+// }
+
+// async function main() {
+//   await app.listen(app.get('port'))
+//   console.log('Server on port', app.get('port'))
+// }
+
+// main()
+const httpsServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl', 'cert.pem')),
+  },
+  app
+)
+httpsServer.listen(3443, () => console.log('Server running at port 3443'))
+
+// const port = 3443
+// https.createServer(httpsOptions, app).listen(port, function(){
+//   console.log(`Server is running at port ${port}`)
+// })
+
+// console.log('Path', path.join(__dirname))
